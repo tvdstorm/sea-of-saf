e6d032e5cac1e2d9db41487c156a525e55fd2821
daansmit.uva@gmail.com <daansmit.uva@gmail.com@e25deb68-5119-325c-d63b-03bbf1c8524c>
2012-01-10 20:47:05 +0000

    fixed Makefile, added command-line arguments to specify fighter

--- daansmit/src/SAF.java
+++ daansmit/src/SAF.java
@@ -5,12 +5,18 @@ import org.antlr.runtime.tree.*;
 public class SAF {
 
     public static void main(String[] args) throws Exception {
-        ANTLRFileStream input = new ANTLRFileStream("..\\data\\fighters\\Simple.saf");
-        SAFLexer lexer = new SAFLexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        SAFParser parser = new SAFParser(tokens);
-        SAFParser.bot_return r = parser.bot();
-        System.out.println("tree = " + ((Tree)r.tree).toStringTree());
+        for (String s: args) {
+            String fighter = "../data/fighters/" + s + ".saf";
+
+            ANTLRFileStream input = new ANTLRFileStream(fighter);
+            SAFLexer lexer = new SAFLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            SAFParser parser = new SAFParser(tokens);
+            SAFParser.bot_return r = parser.bot();
+
+            System.out.println(s);
+            System.out.println("tree = " + ((Tree)r.tree).toStringTree());
+        }
     }
 
 }
