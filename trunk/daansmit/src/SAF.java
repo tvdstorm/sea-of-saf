baa3a9a947d92d026951d84a043a0eb3b13174c9
daansmit.uva@gmail.com <daansmit.uva@gmail.com@e25deb68-5119-325c-d63b-03bbf1c8524c>
2012-01-10 21:46:00 +0000

    Updated grammar to generate a tree and added choose construct, update parser to produce DOT output

--- trunk/daansmit/src/SAF.java
+++ trunk/daansmit/src/SAF.java
@@ -1,21 +1,27 @@
 import org.antlr.runtime.*;
 import org.antlr.runtime.tree.*;
+import org.antlr.stringtemplate.*;
 
 
 public class SAF {
 
     public static void main(String[] args) throws Exception {
         for (String s: args) {
+            // System.out.println(s);
             String fighter = "../data/fighters/" + s + ".saf";
 
             ANTLRFileStream input = new ANTLRFileStream(fighter);
             SAFLexer lexer = new SAFLexer(input);
             CommonTokenStream tokens = new CommonTokenStream(lexer);
             SAFParser parser = new SAFParser(tokens);
-            SAFParser.bot_return r = parser.bot();
+            SAFParser.bot_return bot = parser.bot();
 
-            System.out.println(s);
-            System.out.println("tree = " + ((Tree)r.tree).toStringTree());
+            CommonTree tree = (CommonTree)bot.getTree();
+            DOTTreeGenerator generator = new DOTTreeGenerator();
+            StringTemplate dot = generator.toDOT(tree);
+            System.out.println(dot);
+
+            // System.out.println("tree = " + ((Tree)r.tree).toStringTree());
         }
     }
 
