f967c7642b1e9e746de92072902ba4abf8ceba74
daansmit.uva@gmail.com <daansmit.uva@gmail.com@e25deb68-5119-325c-d63b-03bbf1c8524c>
2012-01-14 10:57:09 +0000

    Added choose construct. Added a fighter class.

--- trunk/daansmit/src/SAF.java
+++ trunk/daansmit/src/SAF.java
@@ -3,26 +3,50 @@ import org.antlr.runtime.tree.*;
 import org.antlr.stringtemplate.*;
 
 
-public class SAF {
-
-    public static void main(String[] args) throws Exception {
-        for (String s: args) {
-            // System.out.println(s);
-            String fighter = "../data/fighters/" + s + ".saf";
-
-            ANTLRFileStream input = new ANTLRFileStream(fighter);
-            SAFLexer lexer = new SAFLexer(input);
-            CommonTokenStream tokens = new CommonTokenStream(lexer);
-            SAFParser parser = new SAFParser(tokens);
-            SAFParser.bot_return bot = parser.bot();
-
-            CommonTree tree = (CommonTree)bot.getTree();
-            DOTTreeGenerator generator = new DOTTreeGenerator();
-            StringTemplate dot = generator.toDOT(tree);
-            System.out.println(dot);
-
-            // System.out.println("tree = " + ((Tree)r.tree).toStringTree());
+public class SAF
+{
+    public static void main(String[] args) throws Exception
+    {
+        for (String s: args)
+        {
+            String path = "../data/fighters/" + s + ".saf";
+            Fighter fighter = Fighter.fromFile(path);
+            
+            System.out.println(fighter.dot);
         }
     }
+}
 
+
+class Fighter
+{
+    public StringTemplate dot;
+    public Personality personality;
+    public Behaviour behaviour;
+
+    public Fighter() { }
+
+    public static Fighter fromFile(String path)
+        throws java.io.IOException, RecognitionException
+    {
+        ANTLRFileStream input = new ANTLRFileStream(path);
+        return Fighter.fromCharStream(input);
+    }
+
+    private static Fighter fromCharStream(CharStream stream)
+        throws RecognitionException
+    {
+        SAFLexer lexer = new SAFLexer(stream);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        SAFParser parser = new SAFParser(tokens);
+        SAFParser.bot_return bot = parser.bot();
+        CommonTree tree = (CommonTree)bot.getTree();
+
+        DOTTreeGenerator generator = new DOTTreeGenerator();
+        StringTemplate dot = generator.toDOT(tree);
+
+        Fighter fighter = new Fighter();
+        fighter.dot = dot;
+        return fighter;
+    }
 }
