b3ebcb524f97757c4d2f6ac9af9f01737ebf2e1c
daansmit.uva@gmail.com <daansmit.uva@gmail.com@e25deb68-5119-325c-d63b-03bbf1c8524c>
2012-01-18 13:26:15 +0000

    Added a checker to validate the tree.

--- daansmit/src/SAF.java
+++ daansmit/src/SAF.java
@@ -1,50 +1,158 @@
-import org.antlr.runtime.*;
-import org.antlr.runtime.tree.*;
-import org.antlr.stringtemplate.*;
+import org.antlr.runtime.tree.CommonTree;
+import org.antlr.runtime.tree.Tree;
+import org.antlr.runtime.CommonToken;
+import org.antlr.runtime.Token;
 
 
-public class SAF
+class SAFException extends org.antlr.runtime.RecognitionException
 {
-    public static void main(String[] args) throws Exception
+    public String message;
+
+    public SAFException(String message)
     {
-        for (String s: args)
-        {
-            String path = "../data/fighters/" + s + ".saf";
-            Fighter fighter = Fighter.fromFile(path);
-            
-            System.out.println(fighter.dot);
-        }
+        this.message = message;
+    }
+
+    public String toString()
+    {
+        return message;
     }
 }
 
 
-class Fighter
+class SAFTree extends CommonTree
 {
-    public StringTemplate dot;
+    public SAFTree() { }
 
-    public Fighter() { }
+    public SAFTree(SAFTree node) { }
 
-    public static Fighter fromFile(String path)
-        throws java.io.IOException, RecognitionException
+    public SAFTree(Token t)
     {
-        ANTLRFileStream input = new ANTLRFileStream(path);
-        return Fighter.fromCharStream(input);
+        token = t;
     }
 
-    private static Fighter fromCharStream(CharStream stream)
-        throws RecognitionException
+    public SAFTree(int ttype)
     {
-        SAFLexer lexer = new SAFLexer(stream);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        SAFParser parser = new SAFParser(tokens);
-        SAFParser.bot_return bot = parser.bot();
-        CommonTree tree = (CommonTree)bot.getTree();
-
-        DOTTreeGenerator generator = new DOTTreeGenerator();
-        StringTemplate dot = generator.toDOT(tree);
-
-        Fighter fighter = new Fighter();
-        fighter.dot = dot;
-        return fighter;
+        token = new CommonToken(ttype, "<" + this.getClass().getName() + ">");
     }
+
+    public SAFTree(int ttype, Token id)
+    {
+        token = new CommonToken(ttype, id.getText());
+    }
+
+    public SAFTree(int ttype, String id)
+    {
+        token = new CommonToken(ttype, id);
+    }
+
+    public boolean isWelformed()
+    {
+        return childrenWelformed();
+    }
+
+    public boolean childrenWelformed()
+    {
+        boolean welformed = true;
+        for (Object child : getChildren())
+        {
+            welformed &= ((SAFTree)child).isWelformed();
+        }
+        return welformed;
+    }
+
+    public String toString()
+    {
+        return token.getText() + "<" + this.getClass().getName() + ">";
+    }
+}
+
+
+class Bot extends SAFTree
+{
+    public Bot(int ttype, Token id) { super(ttype, id); }
+}
+
+
+class Personality extends SAFTree
+{
+    public Personality(int ttype) { super(ttype); }
+}
+
+class Behaviour extends SAFTree
+{
+    public Behaviour(int ttype) { super(ttype); }
+
+    // public boolean childrenWelformed()
+    // {
+    //     boolean hasAlwaysRule = false;
+    //     for (Object rule : getChildren())
+    //     {
+    //         SAFTree condition = ((SAFTree) rule).getFirstChildWithType(CONDITION);
+    //         hasAlwaysRule |= condition.getChildCount() == 1
+    //             && condition.getChild(0).getText() == "always";
+    //     }
+    //     return hasAlwaysRule && childrenWelformed();
+    // }
+}
+
+class Characteristic extends SAFTree
+{
+    public Characteristic(int ttype, Token id) { super(ttype, id); }
+}
+
+class Digit extends SAFTree
+{
+    public int value;
+
+    public Digit(int ttype, Token value)
+    {
+        this.value = Integer.parseInt(value.getText());
+        token = new CommonToken(ttype, Integer.toString(this.value));
+    }
+}
+
+class Condition extends SAFTree
+{
+    public Condition(int ttype) { super(ttype); }
+}
+
+class Rule extends SAFTree
+{
+    public Rule(int ttype) { super(ttype); }
+}
+
+class And extends SAFTree
+{
+    public And(Token t) { super(t); }
+}
+
+class Or extends SAFTree
+{
+    public Or(Token t) { super(t); }
+}
+
+class State extends SAFTree
+{
+    public State(int ttype, Token id) { super(ttype, id); }
+}
+
+class Action extends SAFTree
+{
+    public Action(int ttype) { super(ttype); }
+}
+
+class Choose extends SAFTree
+{
+    public Choose(Token t) { super(t); }
+}
+
+class MoveAction extends SAFTree
+{
+    public MoveAction(int ttype, Token id) { super(ttype, id); }
+}
+
+class FightAction extends SAFTree
+{
+    public FightAction(int ttype, Token id) { super(ttype, id); }
 }
