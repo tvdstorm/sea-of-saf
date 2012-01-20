df59a12064c7d23c6fd5bb0f7832d5b3c41dafdd
marin.vanbeek@gmail.com <marin.vanbeek@gmail.com@e25deb68-5119-325c-d63b-03bbf1c8524c>
2012-01-20 16:37:56 +0000

    Created unit tests for the parser.

--- trunk/marinvanbeek/tests/ParseTest.java
+++ trunk/marinvanbeek/tests/ParseTest.java
@@ -1,36 +1,78 @@
 import org.testng.annotations.*;
+import org.testng.Reporter;
+
+import org.antlr.runtime.*;
+import org.antlr.runtime.*;
+import org.antlr.runtime.tree.*;
+import org.antlr.stringtemplate.*;
+
+import java.io.IOException;
 
 public class ParseTest
 {
     @BeforeClass
     public void setUp()
     {
+        /* Should set stdout and stderr to Reporter. */
         // code that will be invoked when this test is instantiated
     }
 
-//legal_maximum.saf
-//legal_minimal.saf
+    @AfterClass
+    public void tareDown()
+    {
+    }
+
     @Test(groups = { "legal-fighters" })
     public void legalFilesTest()
     {
+        String[] legalFiles = new String[] {"legal_minimum.saf",
+                                            "legal_maximum.saf"};
+
+        for (String fileName : legalFiles)
+        {
+            safFilesTest(fileName, true);
+        }
     }
 
-//illegal_attribute_name.saf
-//illegal_attribute_value.saf
-//illegal_condition.saf
-//illegal_attack.saf
-//illegal_missing_requirement.saf
-//illegal_move.saf
-//illegal_syntax_tree1.saf
-//illegal_syntax_tree2.saf
     @Test(groups = { "illegal-fighters" })
     public void illegalFilesTest()
     {
+        String[] illegalFiles = new String[] {"illegal_attribute_name.saf",
+                                              "illegal_attribute_value.saf",
+                                              "illegal_condition.saf",
+                                              "illegal_attack.saf",
+                                              "illegal_missing_requirement.saf",
+                                              "illegal_move.saf",
+                                              "illegal_syntax_tree1.saf",
+                                              "illegal_syntax_tree2.saf"};
+
+        for (String fileName : illegalFiles)
+        {
+            safFilesTest(fileName, false);
+        }
     }
 
-    public void safFileTest(String file_name, boolean fails)
+    public void safFilesTest(String fileName, boolean passes)
     {
-        System.out.println("Testing " + file_name + "...");
+        try
+        {
+            System.out.println("Testing " + fileName + "...");
+
+            ANTLRFileStream input = new ANTLRFileStream("safs/" + fileName);
+            SAFLexer lexer = new SAFLexer(input);
+            SAFParser parser = new SAFParser(new CommonTokenStream(lexer));
+            SafTreeNode tree = (SafTreeNode)parser.parse().getTree();
+
+            System.out.println(fileName + " is well formed: " +
+                               tree.isWellFormed());
+            System.out.println("Should pass: " + passes);
+        } catch (IOException e)
+        {
+            System.out.println(e.getMessage());
+        } catch (RecognitionException e)
+        {
+            System.out.println(e.getMessage());
+        } 
     }
 }
 
