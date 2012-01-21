258e6da1405bf63352c4f4718a75796bee0c4922
hhoang.nl@gmail.com <hhoang.nl@gmail.com@e25deb68-5119-325c-d63b-03bbf1c8524c>
2012-01-21 14:11:57 +0000

    Updated grammer file Moved parser to package 'safparser' Created a unit test class to test the SAF parser.

---
+++ huyhoang/saf/src/TestUnits/TestSAF.java
@@ -0,0 +1,100 @@
+package TestUnits;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import java.io.*;
+import java.util.*;
+import safparser.*;
+
+public class TestSAF {
+	
+	@Test
+	public void testWellformedSpecification()
+	{
+		
+		
+		String sample = String.format(	"JackieChan {\r\n" +
+										"	kickPower = 1\r\n" +
+										"	punchPower = 10\r\n" +
+										"	kickReach = 1\r\n" +
+										"	punchReach = 5\r\n" +
+										"	far [run_towards kick_low]\r\n" +
+										"	near [crouch choose(punch_low kick_low)]\r\n" +
+										"	weaker [choose(run_away walk_away) choose(block_high block_low)]" +
+										"}" );
+		InputStream stream = new ByteArrayInputStream(sample.getBytes());
+		
+		saf parser = new saf(stream);
+		try
+		{
+			parser.Start();
+		}
+		catch (ParseException exception)
+	    {
+			fail("Code should not have failed");
+	    }
+	}
+	
+	@Test
+	public void testInvalidProperty()
+	{
+		String sample = String.format(	"ChuckNorris {\r\n" +
+										"	throwPower = 1" +
+										"}" );
+		InputStream stream = new ByteArrayInputStream(sample.getBytes());
+		saf parser = new saf(stream);
+		try
+		{
+			parser.Start();
+			fail("Code should have raised a ParseException exception");
+		}
+		catch (ParseException exception)
+		{
+		}
+	}
+	
+	@Test
+	public void testPropertyValidValues() throws Exception {
+		for (int counter=1; counter<11; counter++)
+		{
+			String sample = String.format("JackieChan { kickPower = %d }", counter);
+			InputStream stream = new ByteArrayInputStream(sample.getBytes());
+			
+			saf parser = new saf(stream);
+		    try
+		    {
+		    	parser.Start();
+		    }
+		    catch (ParseException exception)
+		    {
+		    	fail(String.format("Value %d should not cause an exception: %s", counter, sample));
+		    }
+		}
+	}
+
+
+	@Test
+	public void testPropertyInvalidEdgeValues() throws Exception {
+		List<Integer> invalidValues = new ArrayList<Integer>(Arrays.asList(0, 11));
+		
+		for (int invalidValue : invalidValues)
+		{
+			String sample = String.format("JackieChan { kickPower = %d }", invalidValue);
+			InputStream stream = new ByteArrayInputStream(sample.getBytes());
+			
+			saf parser = new saf(stream);
+			try
+		    {
+		    	parser.Start();
+		    	fail(String.format("Value %d should have caused an exception: %s", invalidValue, sample));
+		    }
+		    catch (ParseException exception)
+		    {
+		    }
+			catch (TokenMgrError exception)
+			{
+			}
+		}
+	}
+}
