3271aadfae901c2ec4ad16fe82468b5f71716c0e
marin.vanbeek@gmail.com <marin.vanbeek@gmail.com@e25deb68-5119-325c-d63b-03bbf1c8524c>
2012-01-21 17:03:47 +0000

    Continued on the backend test.

--- trunk/marinvanbeek/tests/FightTest.java
+++ trunk/marinvanbeek/tests/FightTest.java
@@ -1,11 +1,36 @@
 import org.testng.annotations.*;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.lang.Long;
+
 public class FightTest
 {
     @BeforeClass
     public void setUp()
     {
         // code that will be invoked when this test is instantiated
+    }
+
+    public SafFighter getRandomFighter(long seed)
+    {
+        Random randomGenerator = new Random(seed);
+
+        ArrayList<String> legalAttributes = SafAttribute.LEGAL;
+        ArrayList<String> attributes = new ArrayList<String>();
+        for (String legalAttribute : legalAttributes)
+        {
+            int value = randomGenerator.nextInt(SafAttribute.MAX_VALUE) + 1;
+            attributes.add(new SafAttribute(legalAttribute, value));
+        }
+
+        /* FIXME safbehaviour constructor not defined at time of writing. */
+        SafBehaviour behaviour = new SafBehaviour();
+
+        return new SafFighter(Long.toString(seed),
+                              SafFighter.MAX_HEALTH,
+                              attributes.toArray(),
+                              behaviour);
     }
 
     @Test(groups = { "legal-fighters" })
