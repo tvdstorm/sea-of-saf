5da18b80fa572a295192841a613fdea49d00890d
serban.gavrus@gmail.com <serban.gavrus@gmail.com@e25deb68-5119-325c-d63b-03bbf1c8524c>
2012-01-22 20:20:06 +0000

    I am tired

--- trunk/serbangavrus/src/saf/Fighter.java
+++ trunk/serbangavrus/src/saf/Fighter.java
@@ -15,41 +15,50 @@ import antlrgenerated.*;
 public class Fighter
 {
 
+	private CommonTree tree;
+	
+	// Path to "dot.exe" - the app we use to generate a PNG from dot source
+	private final static String dot_path = "C:\\Program Files\\Graphviz 2.28\\bin\\dot.exe";
+	
+	
 	public Fighter(CharStream cs)
 	{
+		// Get the ANTLR generated lexer and parser
 		SAFLexer lexer = new SAFLexer(cs);
 		CommonTokenStream tokens = new CommonTokenStream();
 		tokens.setTokenSource(lexer);
 		SAFParser parser = new SAFParser(tokens);
 		try
 		{
-			SAFParser.fighter_return fighter = parser.fighter();
-			CommonTree tree = (CommonTree)fighter.getTree();
-			System.out.println("tree="+tree.toStringTree());
-			
-			DOTTreeGenerator dottreegen = new DOTTreeGenerator();
-	        StringTemplate dot = dottreegen.toDOT(tree);
-	        DOTTreeGenerator gen = new DOTTreeGenerator();
-	        StringTemplate st = gen.toDOT(tree);
-	        
-	        try {
-				FileWriter dotfile = new FileWriter("dot_graph.dot");
-				dotfile.write(st.toString());
-				dotfile.close();
-				String[] cmd = {"C:\\Program Files\\Graphviz 2.28\\bin\\dot.exe", "-Tpng", "-o", "dot_graph.png", "dot_graph.dot"};
-				Process proc = Runtime.getRuntime().exec(cmd);
-			} catch (IOException e) {
-				System.out.println("Error when trying to write the .dot file.");
-				e.printStackTrace();
-			}
-	        
-			
+			SAFParser.fighter_return fighter = parser.fighter();			
+			tree = (CommonTree)fighter.getTree();
 		}
 		catch (RecognitionException e)
         {
             System.out.println(e.getMessage());
         }
 	}
-
+	
+	
+	// Creates dot and png files from our CommonTree
+	public void exportTree(String tofile)
+	{
+		// Generate a dot tree from our CommonTree
+        DOTTreeGenerator gen = new DOTTreeGenerator();
+        StringTemplate st = gen.toDOT(tree);
+        
+        try {
+        	// The app that generates the PNG needs the dot data to be in a file so we create that file
+			FileWriter dotfile = new FileWriter(tofile + ".dot");
+			dotfile.write(st.toString());
+			dotfile.close();
+			
+			// Generate the PNG using dot.exe
+			String[] cmd = {dot_path, "-Tpng", "-o", tofile + ".png", tofile + ".dot"};
+			Process proc = Runtime.getRuntime().exec(cmd);
+		} catch (IOException e) {
+			System.out.println("Error when trying to create the AST graph.");
+		}
+	}
 	
 }
--- trunk/serbangavrus/src/saf/SAF.java
+++ trunk/serbangavrus/src/saf/SAF.java
@@ -1,6 +1,7 @@
 package saf;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -10,24 +11,50 @@ import org.antlr.runtime.ANTLRFileStream;
 public class SAF
 {
 
+	// Name of the folder where the files that hold the fighter definitions are
 	private static final String PATH_PRE = "data/";
 	
 	public static void main(String[] args)
 	{
-		System.out.println("Enter fighter data file:");
+		String file_path = getFighterFile();
+		try
+		{
+			System.out.println("Loading fighter..");
+			Fighter fighter = new Fighter(new ANTLRFileStream(file_path));
+			System.out.println("Exporting AST..");
+			fighter.exportTree("treegraph");
+			System.out.println("Done.");
+		}
+		catch(FileNotFoundException e)
+		{
+			System.out.println("File not found.");
+		}
+		catch(IOException e)
+		{
+			System.out.println("Error reading fighter file.");
+		}
+	}
+	
+	
+	// Asks the user for the filename where the fighter data is stored
+	private static String getFighterFile()
+	{
+		String file_path = "";
+
+		System.out.println("Enter fighter data filename:");
 		
 		try
 		{
 		    BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
 		    String file_name = bufferRead.readLine();
-		    String file_path = PATH_PRE + file_name;
-		    Fighter fighter = new Fighter(new ANTLRFileStream(file_path));
+		    file_path = PATH_PRE + file_name;
 		}
 		catch(IOException e)
 		{
-			System.out.println("Error reading user input.");
-			e.printStackTrace();
+			System.out.println("Error while reading input.");
 		}
+		
+		return file_path;
+		
 	}
-
 }
