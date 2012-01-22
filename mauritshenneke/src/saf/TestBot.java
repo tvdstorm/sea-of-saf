480c0a48aa5380307a4dfe5aafc2150238c63b39
maurits.henneke@gmail.com <maurits.henneke@gmail.com@e25deb68-5119-325c-d63b-03bbf1c8524c>
2012-01-22 22:58:17 +0000

    * Added a new version with all the correct files and only the src directory.  * Added the bin and the antrl-generated files to the ignore list.  * Created Java evaluator files.  * Created a TestBot file.  * Added return values to the Bot grammer.

---
+++ mauritshenneke/src/saf/TestBot.java
@@ -0,0 +1,34 @@
+package saf;
+
+import org.antlr.runtime.ANTLRStringStream;
+import org.antlr.runtime.CharStream;
+import org.antlr.runtime.CommonTokenStream;
+import org.antlr.runtime.RecognitionException;
+import org.antlr.runtime.TokenStream;
+import saf.evaluators.*;
+
+
+public class TestBot {
+
+	public static void main(String[] args) throws RecognitionException {
+		CharStream charStream = new ANTLRStringStream( "JackieChan \n" +
+										                "{ \n" +
+										                "kickPower = 7 \n" +
+										                "punchPower = 5 \n" +
+										                "kichReach = 3 \n" +
+										                "punchReach = 4 \n" +
+										                "far[run_towards punch_high] \n" +
+										                "near[choose(stand crouch) kick_high] \n     "+
+										                "much_stronger[walk_towards punch_low] \n     "+
+										                "weaker[run_away choose(block_high block_low)] \n     "+
+										                "always[walk_toward block_high] \n     "+
+										                "} \n");
+        BotLexer lexer = new BotLexer(charStream);
+        TokenStream tokenStream = new CommonTokenStream(lexer);
+        BotParser botParser = new BotParser(tokenStream);
+        Bot bot = botParser.create();
+        System.out.println(bot.toString());
+        System.out.println("Done!");
+	}
+
+}
