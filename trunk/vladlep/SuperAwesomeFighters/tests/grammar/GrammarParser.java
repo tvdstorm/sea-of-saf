da3ec06647f7a36e97eb560d412de18697781fe2
vlad.lep@gmail.com <vlad.lep@gmail.com@e25deb68-5119-325c-d63b-03bbf1c8524c>
2012-01-22 12:34:51 +0000

    set up the test environment and project structure

---
+++ trunk/vladlep/SuperAwesomeFighters/tests/grammar/GrammarParser.java
@@ -0,0 +1,40 @@
+package grammar;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.antlr.runtime.ANTLRFileStream;
+import org.antlr.runtime.CharStream;
+import org.antlr.runtime.CommonTokenStream;
+import org.antlr.runtime.RecognitionException;
+import org.antlr.runtime.TokenStream;
+import org.junit.Test;
+
+import reader.antlr.SAFLexer;
+import reader.antlr.SAFParser;
+
+public class GrammarParser {
+
+	@Test
+	public void parseNormalGrammar() {
+		String inputFile = "." + File.separator+ "tests" +File.separator+"grammar"+File.separator+ "fighters.txt";
+		CharStream chStream = null;
+		try {
+			chStream = new ANTLRFileStream(inputFile);
+		} catch (IOException e1) {
+			fail("Could not find or read the grammar file");
+		}
+		SAFLexer lexer = new SAFLexer(chStream);
+		TokenStream tokenStream = new CommonTokenStream(lexer);
+		SAFParser parser = new SAFParser(tokenStream);
+		try {
+			parser.rule();
+		} catch (RecognitionException e) {
+			fail("Could not parse correcltly the grammar");
+		}
+	
+	}
+
+}
