b2f7692e773e69ef40a29126c1d97c75c09f3aa0
job.jonkergouw@gmail.com <job.jonkergouw@gmail.com@e25deb68-5119-325c-d63b-03bbf1c8524c>
2012-01-22 18:15:32 +0000

    Eerste commit

---
+++ trunk/jobjonkergouw/output/SAFTests.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import org.testng.TestNG;
+import org.testng.annotations.*;
+import org.antlr.*;
+import org.antlr.runtime.*;
+
+import java.util.*;
+
+public class SAFTests {
+    @Test
+    public void testNewline() throws IOException, RecognitionException {
+        SAFParser parser = createParser("Botnaam{}");
+        String result = parser.fighter();
+        assert result.equals("Botnaam") : "Expected Botnaam, but got " + result;
+    }
+ 
+    private SAFParser createParser(String testString) throws IOException {
+        CharStream stream = new ANTLRStringStream(testString);
+        SAFLexer lexer = new SAFLexer(stream);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        SAFParser parser = new SAFParser(tokens);
+        return parser;
+    }
+}
