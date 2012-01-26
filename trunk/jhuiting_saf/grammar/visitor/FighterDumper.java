098074574c677098f8fb1eb30f7090ae694df873
joshuiting4@gmail.com <joshuiting4@gmail.com@e25deb68-5119-325c-d63b-03bbf1c8524c>
2012-01-26 12:17:48 +0000

    Updated grammar, created a bunch of new classes

--- trunk/jhuiting_saf/grammar/visitor/FighterDumper.java
+++ trunk/jhuiting_saf/grammar/visitor/FighterDumper.java
@@ -1,18 +1,33 @@
 package visitor;
 
 import java.io.OutputStream;
+
 import java.io.PrintStream;
+
+import ast.*;
 
 public class FighterDumper extends FighterVisitor {
 
-	public void visit(Fighter.FighterElement i) {
+	public void visit(INode i) {
 		System.out.print(i);
 	}
 	
-	public void visit(Fighter.Personality p) {
+	public void visit(Personality p) {
 		System.out.print(p);
 	}
-	public void visit(Fighter.Identifier i) {
+	public void visit(Identifier i) {
 		System.out.print(i);
+	}
+
+	@Override
+	public void startVisit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void finishVisit() {
+		// TODO Auto-generated method stub
+		
 	}
 }
--- trunk/jhuiting_saf/grammar/visitor/FighterVisitor.java
+++ trunk/jhuiting_saf/grammar/visitor/FighterVisitor.java
@@ -1,31 +1,38 @@
 package visitor;
 
+import ast.*;
 import java.util.Iterator;
 
 public abstract class FighterVisitor {
 	
+	public abstract  void startVisit();
+	public abstract void finishVisit();
 
-    public void visit(Fighter f) {
+    public void visit(FighterTree f) {
+    	startVisit(); 
         visit(f.getOptions());
+        finishVisit();
     }
 	
-	public void visit(Fighter.Identifier identifier) {
+	public void visit(Identifier identifier) {
 		
 	}
 	
-    public void visit(Fighter.Options s) {
-        for (Iterator iterator = s.iterator(); iterator.hasNext();) {
-            Fighter.FighterElement fEl = (Fighter.FighterElement) iterator.next();
+    public void visit(Options s) {
+        for (Iterator<INode> iterator = s.iterator(); iterator.hasNext();) {
+        	INode fEl = iterator.next();
             fEl.accept(this);
-        }
+        } 
     }
 
-	public void visit(Fighter.Personality personality) {
+	public void visit(Personality personality) {
 		
 	}
 
-	public void visit(Fighter.Characteristic characteristic) {
-		// TODO Auto-generated method stub
+	public void visit(Characteristic characteristic) {
+		
+	}
+	public void visit(Action action) {
 		
 	}
 }
