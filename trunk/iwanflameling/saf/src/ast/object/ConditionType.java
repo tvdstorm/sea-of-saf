896a34f14c12c2eb69855c24a0a35353fabe7be8
iwanovich@gmail.com <iwanovich@gmail.com@e25deb68-5119-325c-d63b-03bbf1c8524c>
2012-01-30 15:30:04 +0000

    created  a few AST.objects for my resulting AST tree generated by the parser-to-be. also created a little bit of structure in the project (parser and ast packages). saf.jj is a generated JavaCC grammar file to start developing with.

---
+++ trunk/iwanflameling/saf/src/ast/object/ConditionType.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package ast.object;
+
+/**
+ * {@link ConditionType}'s that describe a condition a {@link Fighter} finds itself in.
+ * @author Iwan Flameling
+ */
+public enum ConditionType {
+	ALWAYS,
+	NEAR,
+	FAR,
+	MUCH_STRONGER,
+	STRONGER,
+	EVEN,
+	WEAKER,
+	MUCH_WEAKER
+}
