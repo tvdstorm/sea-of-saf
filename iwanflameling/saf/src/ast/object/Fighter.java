40522299a97ec363f630271f4f25020dfcbef40f
iwanovich@gmail.com <iwanovich@gmail.com@e25deb68-5119-325c-d63b-03bbf1c8524c>
2012-01-30 16:40:10 +0000

    added fighter charecteristics

--- iwanflameling/saf/src/ast/object/Fighter.java
+++ iwanflameling/saf/src/ast/object/Fighter.java
@@ -3,13 +3,43 @@
  */
 package ast.object;
 
+import java.util.Set;
+
 /**
  * Describes a Fighter in the SAF game.
  * @author Iwan Flameling
  */
 public class Fighter {
-	
+	/**
+	 * The name of this {@link Fighter}.
+	 */
 	private String name;
+	
+	/**
+	 * A {@link Set} of {@link Rule}s to describe the behavior of this {@link Fighter}. 
+	 */
+	private Set<Rule> behaviour;
+	
+	/**
+	 * Indication of how far this {@link Fighter} can reach its punch.
+	 */
+	private int punchReach;
+	
+	/**
+	 * Indication of how far this {@link Fighter} can reach its kick.
+	 */
+	private int kickReach;
+	
+	/**
+	 * Indication of how powerful this {@link Fighter} can punch.
+	 */
+	private int punchPower;
+	
+	/**
+	 * Indication of kick-power on a scale of 1 to 10 inclusive.
+	 */
+	private int kickPower;
+	
 	
 	/**
 	 * no-arg constructor, because I don't know yet the mandatory
