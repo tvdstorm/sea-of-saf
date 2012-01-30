75b157b463e52204d240b2a1e6131b5cd7dab221
daansmit.uva@gmail.com <daansmit.uva@gmail.com@e25deb68-5119-325c-d63b-03bbf1c8524c>
2012-01-30 08:12:39 +0000

    Added tests and run scripts.

--- trunk/daansmit/src/SAFTest.java
+++ trunk/daansmit/src/SAFTest.java
@@ -1,20 +1,57 @@
 import org.antlr.runtime.*;
-import org.junit.Test;
+import org.junit.*;
 
+import java.io.PrintStream;
+import java.io.FileNotFoundException;
 
 public class SAFTest
 {
-    @Test
-    public void testAlwaysRule() throws RecognitionException
+    private static final String defaultAlwaysRule = 
+        "always [crouch block_low]";
+
+    @BeforeClass
+    public static void setup()
     {
-        String input = "Always { always [walk_towards punch_high] }";
+        try
+        {
+            PrintStream log = new PrintStream("test.out");
+            System.setOut(log);
+            System.setErr(log);
+        }
+        catch (FileNotFoundException e)
+        {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testAlwaysRule()
+        throws RecognitionException
+    {
+        String input = "Always { " + alwaysRule + " }";
         Fighter fighter = new Fighter(new ANTLRStringStream(input));
     }
 
     @Test(expected = SAFException.class)
-    public void testMissingAlwaysRule() throws java.io.IOException, RecognitionException
+    public void testMissingAlwaysRule()
+        throws java.io.IOException, RecognitionException
     {
         String input = "NoAlwaysRule { far [walk_towards punch_high] }";
+        Fighter fighter = new Fighter(new ANTLRStringStream(input));
+    }
+
+    public void validNumber()
+        throws RecognitionException
+    {
+        String input = "ValidNumber { punchReach = 1 " + defaultAlwaysRule;
+        Fighter fighter = new Fighter(new ANTLRStringStream(input));
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void testInvalidNumber()
+        throws RecognitionException
+    {
+        String input = "InvalidNumber { punchReach = a " + defaultAlwaysRule;
         Fighter fighter = new Fighter(new ANTLRStringStream(input));
     }
 }
