af7daf8432321b61a281b57af813854e57dfc385
mail@koenhanselman.nl <mail@koenhanselman.nl@e25deb68-5119-325c-d63b-03bbf1c8524c>
2012-01-31 12:31:36 +0000

    converted to antlr

--- trunk/koenhanselman/EclipseWorkspace/SAFParser/src/Main.java
+++ trunk/koenhanselman/EclipseWorkspace/SAFParser/src/Main.java
@@ -7,26 +7,25 @@ import saf.node.* ;
 import java.io.* ; 
   
 public class Main { 
-   public static void main(String[] args) { 
-      if (args.length > 0) { 
-         try { 
-            /* Form the AST */ 
-            Lexer lexer = new Lexer (new PushbackReader( 
-               new FileReader(args[0]), 1024)); 
-            Parser parser = new Parser(lexer); 
-            Start ast = parser.parse() ; 
-  
-            /* Start the interpreter */ 
-            Interpreter interp = new Interpreter () ; 
-            ast.apply(interp) ; 
-            System.out.println (ast) ;
-         } 
-         catch (Exception e) { 
-            System.out.println (e) ; 
-         } 
-      } else { 
-         System.err.println("usage: java simpleAdder inputFile"); 
-         System.exit(1); 
-      } 
-   } 
-}
+	   public static void main(String[] args) { 
+	      if (args.length > 0) { 
+	         try { 
+	            /* Form the AST */ 
+	            Lexer lexer = new Lexer (new PushbackReader( 
+	               new FileReader(args[0]), 1024)); 
+	            Parser parser = new Parser(lexer); 
+	            Start ast = parser.parse() ; 
+	  
+	            /* Start the interpreter */ 
+	            Interpreter interp = new Interpreter () ; 
+	            ast.apply(interp) ; 
+	         } 
+	         catch (Exception e) { 
+	            System.out.println (e) ; 
+	         } 
+	      } else { 
+	         System.err.println("usage: java simpleAdder inputFile"); 
+	         System.exit(1); 
+	      } 
+	   } 
+	}
---
+++ trunk/koenhanselman/EclipseWorkspace/SAFParser/src/saf/interpret/condition_type.java
@@ -0,0 +1,12 @@
+package saf.interpret;
+
+public enum condition_type {
+	always,
+	near,
+	far,
+	much_stronger,
+	stronger,
+	even,
+	weaker,
+	much_weaker
+}
