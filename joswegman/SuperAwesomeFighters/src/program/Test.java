675244fb5d9d2b7ebe35a3967d24a89674f27e62
jos.wegman <jos.wegman@e25deb68-5119-325c-d63b-03bbf1c8524c>
2012-02-02 12:07:51 +0000

    Saf declaraties in bestanden gezet en uit  Test.java gehaald

--- joswegman/SuperAwesomeFighters/src/program/Test.java
+++ joswegman/SuperAwesomeFighters/src/program/Test.java
@@ -1,4 +1,5 @@
 package program;
+import java.io.IOException;
 
 import org.antlr.runtime.ANTLRStringStream;
 
@@ -6,6 +7,7 @@ import org.antlr.runtime.CharStream;
 import org.antlr.runtime.CommonTokenStream;
 import org.antlr.runtime.RecognitionException;
 import org.antlr.runtime.TokenStream;
+import org.antlr.runtime.*;
 
 import grammar.SafLexer;
 import grammar.SafParser;
@@ -15,43 +17,20 @@ import grammar.Evaluators.*;
 public class Test {
 
 	
-	public static void main(String[] args) throws RecognitionException {
-		CharStream charStream = new ANTLRStringStream( "Sample \n" +
-				"{ \n" +
-				"kickPower = 6 " +
-				"punchReach = 4 \n" +
-				"stronger near or far [ stand punch_low] \n" +
-				"stronger [run_towards block_high] \n" +
-				"always [ choose (jump stand) kick_high] \n	"+
-				"} \n");
-		SafLexer lexer = new SafLexer(charStream);
+	public static void main(String[] args) throws RecognitionException, IOException {
+		ANTLRFileStream fileStream = new ANTLRFileStream ("/home/jos/workspace/SuperAwesomeFighters/src/program/Sample.saf");
+		SafLexer lexer = new SafLexer(fileStream);
 		TokenStream tokenStream = new CommonTokenStream(lexer);
 		SafParser parser = new SafParser(tokenStream);
-		//parser.bot();
 		Bot b = parser.bot();
 		b.accept(new BotChecker());
-		System.out.println("done: " + b.toString());
-		System.out.println();
-
 		
-		CharStream charStream1 = new ANTLRStringStream( "Test \n" +
-				"{ \n" +
-				"kickPower = 2 \n" +
-				"punchReach = 8 \n" +
-				"near [ walk_towards punch_high] \n" +
-				"stronger [run_away block_low] \n" +
-				"always and far [ choose (jump stand) kick_high] \n	"+
-				"} \n");
-		SafLexer lexer1 = new SafLexer(charStream1);
+		ANTLRFileStream fileStream1 = new ANTLRFileStream("/home/jos/workspace/SuperAwesomeFighters/src/program/Test.saf");
+		SafLexer lexer1 = new SafLexer(fileStream1);
 		TokenStream tokenStream1 = new CommonTokenStream(lexer1);
 		SafParser parser1 = new SafParser(tokenStream1);
-		//parser1.bot();
 		Bot b1 = parser1.bot();
 		b1.accept(new BotChecker());
-		System.out.println("done: " + b1.toString());
-		System.out.println();
-
-		
 	}
 
 }
