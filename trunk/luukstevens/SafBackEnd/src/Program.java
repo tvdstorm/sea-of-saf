549cb46fa3e19cfdb339f5497b25f92e23013afe
luuk.stevens@gmail.com <luuk.stevens@gmail.com@e25deb68-5119-325c-d63b-03bbf1c8524c>
2012-02-02 17:04:48 +0000

    First fight logic

--- trunk/luukstevens/SafBackEnd/src/Program.java
+++ trunk/luukstevens/SafBackEnd/src/Program.java
@@ -2,12 +2,9 @@
 import java.io.FileNotFoundException;
 import javax.xml.bind.JAXBException;
 
-import view.Arena;
 import view.Main;
 import logic.Fight;
 import model.Bot;
-import model.attack.*;
-import model.move.*;
 
 public class Program {
 
@@ -18,8 +15,8 @@ public class Program {
 	 * @throws InterruptedException 
 	 */
 	public static void main(String[] args) throws FileNotFoundException, JAXBException, InterruptedException {
-		Bot left = Bot.deserialize("/Users/luukstevens/jack.xml");
-		Bot right = Bot.deserialize("/Users/luukstevens/jack.xml");
+		Bot left = Bot.deserialize("/Users/luukstevens/jack.xml", Bot.Position.LEFT);
+		Bot right = Bot.deserialize("/Users/luukstevens/neo.xml", Bot.Position.RIGHT);
 		Main view = new Main(left, right);
 		
 		Fight fight = new Fight();
--- trunk/luukstevens/SafBackEnd/src/model/BehaviourRule.java
+++ trunk/luukstevens/SafBackEnd/src/model/BehaviourRule.java
@@ -1,17 +1,25 @@
 package model;
 
 import java.util.List;
+import java.util.Random;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementRef;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlType;
 
+import model.attack.Attack;
 import model.condition.Condition;
 import model.enums.ConditionType;
+import model.move.Move;
 
 @XmlType(name = "BehaviourRule")
 public class BehaviourRule {
+	private Random randomGenerator;
+	
+	public BehaviourRule() {
+		randomGenerator = new Random();
+	}
 	
 	@XmlElementRef
 	private Condition condition;
@@ -34,6 +42,16 @@ public class BehaviourRule {
 
 	public Condition getCondition() {
 		return condition;
+	}
+	
+	public Move getMove() {
+		int index = randomGenerator.nextInt(getMoveActions().size());
+		return model.move.Factory.create(getMoveActions().get(index).getValue());
+	}
+	
+	public Attack getAttack() {
+		int index = randomGenerator.nextInt(getFightActions().size());
+		return model.attack.Factory.create(getFightActions().get(index).getValue());
 	}
 	
 	public boolean evaluate(List<ConditionType> conditions) {
