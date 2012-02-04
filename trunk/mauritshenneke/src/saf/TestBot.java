8282cf525cc10874b4feebb0a8840ffadf213272
maurits.henneke@gmail.com <maurits.henneke@gmail.com@e25deb68-5119-325c-d63b-03bbf1c8524c>
2012-02-04 15:35:44 +0000

    * Added fighter package.  * implemented fighter instantiation

--- trunk/mauritshenneke/src/saf/TestBot.java
+++ trunk/mauritshenneke/src/saf/TestBot.java
@@ -1,34 +1,42 @@
 package saf;
 
-import org.antlr.runtime.ANTLRStringStream;
-import org.antlr.runtime.CharStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.antlr.runtime.ANTLRFileStream;
 import org.antlr.runtime.CommonTokenStream;
 import org.antlr.runtime.RecognitionException;
 import org.antlr.runtime.TokenStream;
 import saf.evaluators.*;
 
-
 public class TestBot {
+	private static ArrayList<Bot> bots = new ArrayList<Bot>();
 
-	public static void main(String[] args) throws RecognitionException {
-		CharStream charStream = new ANTLRStringStream( "JackieChan \n" +
-										                "{ \n" +
-										                "kickPower = 7 \n" +
-										                "punchPower = 5 \n" +
-										                "kichReach = 3 \n" +
-										                "punchReach = 4 \n" +
-										                "far and weaker[run_towards punch_high] \n" +
-										                "near[choose(stand crouch) kick_high] \n     "+
-										                "much_stronger[walk_towards punch_low] \n     "+
-										                "weaker[run_away choose(block_high block_low)] \n     "+
-										                "always[walk_toward block_high] \n     "+
-										                "} \n");
-        BotLexer lexer = new BotLexer(charStream);
-        TokenStream tokenStream = new CommonTokenStream(lexer);
-        BotParser botParser = new BotParser(tokenStream);
-        Bot bot = botParser.create();
-        System.out.println(bot.toString());
-        System.out.println("Done!");
+	public static void main(String[] args) throws IOException, RecognitionException{
+		for (String fileName : args) {
+			Bot bot = createBot(fileName);
+			addBotToList(bot);
+		}
 	}
-
+	
+	private static Bot createBot(String fileName) throws RecognitionException, IOException{
+		String filePath = String.format("src/saf/fighters/%s.saf", fileName);
+		boolean exists = (new File(filePath)).exists();
+		if(!exists){
+			return null;
+		}
+		
+		ANTLRFileStream fileStream = new ANTLRFileStream(filePath);
+		BotLexer lexer = new BotLexer(fileStream);
+		TokenStream tokenStream = new CommonTokenStream(lexer);
+		BotParser botParser = new BotParser(tokenStream);
+		return botParser.create();
+	}
+	
+	private static void addBotToList(Bot bot){
+		if(bot != null){
+			bots.add(bot);
+		}
+	}
 }
