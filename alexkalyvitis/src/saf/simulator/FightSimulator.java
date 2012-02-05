564f223206e39dcb854c45e6f2aeafdd5dcc22c3
alex.kalyvitis <alex.kalyvitis@e25deb68-5119-325c-d63b-03bbf1c8524c>
2012-02-05 11:21:12 +0000

    working on simulator

--- alexkalyvitis/src/saf/simulator/FightSimulator.java
+++ alexkalyvitis/src/saf/simulator/FightSimulator.java
@@ -3,7 +3,6 @@ package saf.simulator;
 import java.util.List;
 
 import org.lwjgl.LWJGLException;
-//import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
@@ -16,117 +15,87 @@ public class FightSimulator{
 	public FightSimulator(List<Fighter> fighters){
 		fight = new Fight(fighters);
 	}
-	
-//	public void paint(Graphics graphic)
-//    {
-//		Graphics2D g = (Graphics2D) graphic;
-//		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
-//		g.setStroke(new BasicStroke(2));
-		
-//		// stand_still
-//		g.drawOval(75, 125, 50, 75); // Head
-//		g.drawLine(100, 200, 100, 300); // Body
-//		g.drawLine(75, 225, 100, 225); // Left Arm
-//		g.drawLine(125, 225, 100, 225); // Right Arm
-//		g.drawLine(75, 350, 100, 300); // Left Leg
-//		g.drawLine(125, 350, 100, 300); // Right Leg
-//		
-//		// punch_high
-//		g.drawOval(75, 125, 50, 75); // Head
-//		g.drawLine(100, 200, 100, 300); // Body
-//		g.drawLine(75, 250, 100, 225); // Left Arm
-//		g.drawLine(75, 250, 75, 275); // Left Arm
-//		g.drawLine(75, 275, 100, 250); // Left Arm
-//		g.drawLine(100, 225, 150, 175); // Right Arm
-//		g.drawLine(75, 350, 100, 300); // Left Leg
-//		g.drawLine(125, 350, 100, 300); // Right Leg
-//		
-//		// punch_low
-//		g.drawOval(75, 125, 50, 75); // Head
-//		g.drawLine(100, 200, 100, 300); // Body
-//		g.drawLine(75, 200, 100, 225); // Left Arm
-//		g.drawLine(75, 200, 75, 225); // Left Arm
-//		g.drawLine(75, 225, 100, 250); // Left Arm
-//		g.drawLine(100, 225, 150, 275); // Right Arm
-//		g.drawLine(75, 350, 100, 300); // Left Leg
-//		g.drawLine(125, 350, 100, 300); // Right Leg
-		
-//		// kick_high
-//		g.drawOval(75, 125, 50, 75); // Head
-//		g.drawLine(100, 200, 100, 300); // Body
-//		g.drawLine(75, 225, 100, 225); // Left Arm
-//		g.drawLine(125, 225, 100, 225); // Right Arm
-//		g.drawLine(75, 325, 100, 300); // Left Leg
-//		g.drawLine(75, 325, 75, 350); // Left Leg
-//		g.drawLine(75, 350, 100, 325); // Left Leg
-//		g.drawLine(150, 250, 100, 300); // Right Leg
-		
-//		// kick_low
-//		g.drawOval(75, 125, 50, 75); // Head
-//		g.drawLine(100, 200, 100, 300); // Body
-//		g.drawLine(75, 225, 100, 225); // Left Arm
-//		g.drawLine(125, 225, 100, 225); // Right Arm
-//		g.drawLine(75, 300, 100, 300); // Left Leg
-//		g.drawLine(75, 300, 75, 325); // Left Leg
-//		g.drawLine(75, 325, 100, 325); // Left Leg
-//		g.drawLine(150, 300, 100, 300); // Right Leg
-		
-//		while(true){
-//			fight.nextAction();
-//			fight.delay();
-//			g.fill(fight.drawState());
-//			
-//			if(fight.getFirstFighter().getHealth() <= 0 || fight.getSecondFighter().getHealth() <= 0 || new Date().getTime() >= timeIsUp){
-//				break;
-//			}
-//		}
-//    }
-	
+
 	public void Simulate(){
 		System.out.println("Simulating...");
 		
 		try {
-			Display.setDisplayMode(new DisplayMode(800,600));
+			Display.setDisplayMode(new DisplayMode(1024,400));
 			Display.setTitle(fight.getFirstFighter().getName() + " V " + fight.getSecondFighter().getName());
 			Display.create();
 		} catch (LWJGLException e) {
-			e.printStackTrace();
+			System.out.println("Simulation Failed...\n " + e.getMessage());
 			System.exit(0);
 		}
-		// init OpenGL here
+		// init OpenGL
 		GL11.glMatrixMode(GL11.GL_PROJECTION);
 		GL11.glLoadIdentity();
-		GL11.glOrtho(0, 800, 0, 600, 1, -1);
+		GL11.glOrtho(0, 1024, 0, 400, 1, -1);
 		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+		
+		drawBackground();
 		
 		while (!Display.isCloseRequested()) {
 			// render OpenGL here
-			drawPunchHigh();
+			drawStand(0);
+			drawPunchHigh(175);
+			drawPunchLow(350);
+			drawKickHigh(525);
+			drawKickLow(700);
 			Display.update();
 		}
 		Display.destroy();
 		
 		System.out.println("Simulating Finished");
 	}
-
-	public void drawPunchHigh() {
-		circle(200, 250, 50, 100); // Head
-		line(200, 200, 200, 100); // Body
-		line(150, 175, 250, 175);
-		line(150, 50, 200, 100); // Left leg
-		line(250, 50, 200, 100); // Right leg
+	public void drawStand(float initX) {
+		circle(initX + 100, 250, 50); // Head
+		line(initX + 100, 200, initX + 100, 100); // Body
+		line(initX + 50, 175, initX + 150, 175); // Both arms
+		line(initX + 50, 50, initX + 100, 100); // Left leg
+		line(initX + 150, 50, initX + 100, 100); // Right leg
+	}
+	
+	public void drawPunchHigh(float initX) {
+		circle(initX + 100, 250, 50); // Head
+		line(initX + 100, 200, initX + 100, 100); // Body
+		line(initX + 75, 150, initX + 100, 175); // Left arm
+		line(initX + 75, 150, initX + 75, 125); // Left arm
+		line(initX + 75, 125, initX + 100, 150); // Left arm
+		line(initX + 100, 175, initX + 175, 250); // Right arm
+		line(initX + 50, 50, initX + 100, 100); // Left leg
+		line(initX + 150, 50, initX + 100, 100); // Right leg
 	}
 
-	public void drawPunchLow() {
-		
+	public void drawPunchLow(float initX) {
+		circle(initX + 100, 250, 50); // Head
+		line(initX + 100, 200, initX + 100, 100); // Body
+		line(initX + 75, 175, initX + 100, 175); // Left arm
+		line(initX + 75, 175, initX + 75, 150); // Left arm
+		line(initX + 75, 150, initX + 100, 150); // Left arm
+		line(initX + 100, 175, initX + 175, 175); // Right arm
+		line(initX + 50, 50, initX + 100, 100); // Left leg
+		line(initX + 150, 50, initX + 100, 100); // Right leg
 	}
 
-	public void drawKickHigh() {
-		
+	public void drawKickHigh(float initX) {
+		circle(initX + 100, 250, 50); // Head
+		line(initX + 100, 200, initX + 100, 100); // Body
+		line(initX + 75, 150, initX + 125, 200); // Both arms
+		line(initX + 75, 75, initX + 100, 100); // Left leg
+		line(initX + 75, 75, initX + 75, 50); // Left leg
+		line(initX + 75, 50, initX + 100, 75); // Left leg
+		line(initX + 100, 100, initX + 175, 175); // Right leg
 	}
 
-	public void drawKickLow() {
-		
+	public void drawKickLow(float initX) {
+		circle(initX + 100, 250, 50); // Head
+		line(initX + 100, 200, initX + 100, 100); // Body
+		line(initX + 75, 175, initX + 125, 175); // Both arms
+		line(initX + 75, 100, initX + 100, 100); // Left leg
+		line(initX + 75, 100, initX + 75, 75); // Left leg
+		line(initX + 75, 75, initX + 100, 75); // Left leg
+		line(initX + 100, 100, initX + 175, 100); // Right leg
 	}
 
 	public void line(float x1, float y1, float x2, float y2){
@@ -135,6 +104,15 @@ public class FightSimulator{
 		GL11.glVertex2f(x1,y1);
 		GL11.glVertex2f(x2,y2);
 		GL11.glEnd();
+	}
+	public void circle(float x, float y, float r) {
+	    double inc = Math.PI / 12;
+	    double max = 2 * Math.PI;
+	    GL11.glBegin(GL11.GL_LINE_LOOP);
+	    for(double d = 0; d < max; d += inc) {
+	    	GL11.glVertex2f((float)Math.cos(d) * r + x, (float)Math.sin(d) * r + y);
+	    }
+	    GL11.glEnd();
 	}
 	
 	public void circle(float x, float y, float r, int segments){
@@ -145,5 +123,18 @@ public class FightSimulator{
             GL11.glVertex2f(x + (float)Math.sin(t)*r, y + (float)Math.cos(t)*r);
         }
         GL11.glEnd();
+	}
+	
+	public void drawBackground(){
+		GL11.glBegin(GL11.GL_QUADS);
+		GL11.glTexCoord2f(0,0);
+		GL11.glVertex2f(0,0);
+		GL11.glTexCoord2f(1,0);
+		GL11.glVertex2f(1,0);
+		GL11.glTexCoord2f(1,1);
+		GL11.glVertex2f(1,1);
+		GL11.glTexCoord2f(0,1);
+		GL11.glVertex2f(0,1);
+		GL11.glEnd();
 	}
 }
