d349af2a371bf36338298c5ee7392b36bf893676
iwanovich@gmail.com <iwanovich@gmail.com@e25deb68-5119-325c-d63b-03bbf1c8524c>
2012-02-06 17:44:03 +0000

    implemented AND and OR in AST

---
+++ trunk/iwanflameling/saf/src/ast/condition/And.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package ast.condition;
+
+/**
+ * @author iwan
+ *
+ */
+public class And extends Connective {
+	
+	/**
+	 * 
+	 */
+	public And() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ trunk/iwanflameling/saf/src/ast/condition/Leaf.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package ast.condition;
+
+import ast.ASTNode;
+
+/**
+ * @author iwan
+ *
+ */
+public class Leaf extends ASTNode {
+
+	/**
+	 * 
+	 */
+	public Leaf() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
