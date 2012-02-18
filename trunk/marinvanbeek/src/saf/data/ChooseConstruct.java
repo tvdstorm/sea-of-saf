166d1b21e12515da9c5ec7471d34a7149e97f2e2
marin.vanbeek@gmail.com <marin.vanbeek@gmail.com@e25deb68-5119-325c-d63b-03bbf1c8524c>
2012-02-18 16:33:57 +0000

    part of previous commit.

---
+++ trunk/marinvanbeek/src/saf/data/ChooseConstruct.java
@@ -0,0 +1,52 @@
+package saf.data;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ChooseConstruct
+{
+    private List<String> possibilities;
+
+    public ChooseConstruct(String name)
+    {
+        this.possibilities = new ArrayList<String>();
+        possibilities.add(name);
+    }
+
+    public ChooseConstruct(List<String> possibilities)
+    {
+        this.possibilities = possibilities;
+    }
+
+    public String toString()
+    {
+        String stringRepresentation = "";
+
+        if (possibilities.size() == 1)
+        {
+            stringRepresentation = possibilities.get(0);
+        }
+        else
+        {
+            stringRepresentation = "choose ( ";
+            for (String name : possibilities)
+            {
+                stringRepresentation += name + " ";
+            }
+            stringRepresentation += ")";
+        }
+
+        return stringRepresentation;
+    }
+    
+    public String getName()
+    {
+        Random random = new Random();
+        int randomIndex = random.nextInt(possibilities.size());
+
+        return possibilities.get(randomIndex); 
+    }
+    public List<String> getAllPossibilities() { return possibilities; }
+}
+
