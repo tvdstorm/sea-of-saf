231f568a382e9ea146703250b765a3a1b3ee502a
sophia.georgiadou1@gmail.com <sophia.georgiadou1@gmail.com@e25deb68-5119-325c-d63b-03bbf1c8524c>
2012-02-19 17:56:34 +0000

    Close to final version. Visualization working

--- trunk/sophiageorgiadou/src/safGame/safGame.java
+++ trunk/sophiageorgiadou/src/safGame/safGame.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 
 import parser.ParseException;
 import parser.Saf;
+import safVisualization.FighterVisualization;
+import safVisualization.StdDraw;
 
 import abstractSyntaxTree.Fighter;
 import org.lwjgl.LWJGLException;
@@ -20,6 +22,17 @@ public class safGame {
 	int distance = 0;
 	String locationCondition="far";
 	
+	public void initScreen(){
+		StdDraw.setCanvasSize(800, 600);
+		StdDraw.setXscale(0, 10.0);
+		StdDraw.setYscale(0, 10.0);
+		refreshScreen();
+	}
+	
+	public void refreshScreen(){
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.filledSquare(0, 0, 20.0);
+	}
 	
 	public void calculateHealth(Fighter f1, Fighter f2) {
 
@@ -33,9 +46,18 @@ public class safGame {
 				|| (((attack1.equals("punch_high") || attack1
 						.equals("kick_high")) && attack2.equals("block_high")) || ((attack2
 						.equals("punch_high") || attack2.equals("kick_high")) && attack1
-						.equals("block_high")))) {
+						.equals("block_high"))) || (attack1.equals("none") && attack2.equals("none"))) {
 			return;
-		} else {
+		}else if(attack1.equals("none")){
+			if((distance / 10) <= f2.getCurrentAttackReach()){
+				f1.setHealth(f1.getHealth() - f2.getCurrentAttackPower());
+			}
+		}else if(attack2.equals("none")){
+			if((distance / 10) <= f1.getCurrentAttackReach()){
+				f2.setHealth(f2.getHealth() - f1.getCurrentAttackPower());
+			}
+		}
+		else {
 			if ((distance / 10) <= f1.getCurrentAttackReach()) {
 				f2.setHealth(f2.getHealth() - f1.getCurrentAttackPower());
 			}
@@ -46,26 +68,41 @@ public class safGame {
 	}
 
 	public void updateLocation(Fighter f1, Fighter f2) {
+		
+		int temp=0;
+		
 		if (f1.getCurrentMove().equals("run_towards")) {
-			f1.setLocation(f1.getLocation() + ((int) f1.getSpeed() * 2));
+			f1.setLocation(f1.getLocation() + ((int) (f1.getSpeed()+0.5) * 5));
 		} else if (f1.getCurrentMove().equals("walk_towards")) {
-			f1.setLocation(f1.getLocation() + ((int) f1.getSpeed()));
+			f1.setLocation(f1.getLocation() + ((int) (f1.getSpeed()+0.5) * 2));
 		} else if (f1.getCurrentMove().equals("run_away")) {
-			f1.setLocation(f1.getLocation() + ((int) f1.getSpeed() * 2));
+			f1.setLocation(f1.getLocation() + ((int) (f1.getSpeed()+0.5) * 5));
 		} else if (f1.getCurrentMove().equals("walk_away")) {
-			f1.setLocation(f1.getLocation() + ((int) f1.getSpeed()));
+			f1.setLocation(f1.getLocation() + ((int) (f1.getSpeed()+0.5) * 2));
 		}
 
 		if (f2.getCurrentMove().equals("run_towards")) {
-			f2.setLocation(f2.getLocation() + ((int) f2.getSpeed() * 2));
+			f2.setLocation(f2.getLocation() - ((int) (f2.getSpeed()+0.5) * 5));
 		} else if (f2.getCurrentMove().equals("walk_towards")) {
-			f2.setLocation(f2.getLocation() + ((int) f2.getSpeed()));
+			f2.setLocation(f2.getLocation() - ((int) (f2.getSpeed()+0.5) * 2));
 		} else if (f2.getCurrentMove().equals("run_away")) {
-			f2.setLocation(f2.getLocation() + ((int) f2.getSpeed() * 2));
+			f2.setLocation(f2.getLocation() - ((int) (f2.getSpeed()+0.5) * 5));
 		} else if (f2.getCurrentMove().equals("walk_away")) {
-			f2.setLocation(f2.getLocation() + ((int) f2.getSpeed()));
+			f2.setLocation(f2.getLocation() - ((int) (f2.getSpeed()+0.5) * 2));
 		}
 
+		
+		if(f1.getLocation()==f2.getLocation()){
+			f1.setLocation(f1.getLocation()-1);
+			f2.setLocation(f2.getLocation()+1);
+		}
+		
+		else if(f1.getLocation()>f2.getLocation()){
+			temp=f1.getLocation();
+			f1.setLocation(f2.getLocation());
+			f2.setLocation(temp);
+		}
+		
 		if (f1.getLocation() > 90)
 			f1.setLocation(90);
 		else if (f1.getLocation() < 0)
@@ -74,6 +111,8 @@ public class safGame {
 			f2.setLocation(90);
 		else if (f2.getLocation() < 0)
 			f2.setLocation(0);
+		
+		updateLocationCondition(f1,f2);
 	}
 
 	public int calculateDistance(Fighter f1, Fighter f2) {
@@ -159,29 +198,116 @@ public class safGame {
 		System.out.println(fighters);
 
 		fighters.get(0).setLocation(0);
-		fighters.get(0).setLocation(90);
+		fighters.get(1).setLocation(90);
 
 
 		Fighter f1=fighters.get(0);
 		Fighter f2=fighters.get(1);
-				
-/*		int i=0;
-		while(i<10){
-			System.out.println("Play move: "+i);
+		
+		//Graphics
+		game.initScreen();
+		FighterVisualization p1=new FighterVisualization(f1.getHeight(),1,f1.getName(),f1.getLocation());		
+		FighterVisualization p2=new FighterVisualization(f2.getHeight(),2,f2.getName(),f2.getLocation());
+		p1.drawFighter();
+		p2.drawFighter();
+		game.refreshScreen();
+		StdDraw.setPenColor(StdDraw.MAGENTA);
+		StdDraw.text(1, 9.0, f1.getName());
+		StdDraw.text(9.0, 9.0, f2.getName());
+		StdDraw.text(1.0, 9.5, "Health = "+f1.getHealth());
+		StdDraw.text(9.0, 9.5, "Health = "+f2.getHealth());
+		p1.drawFighter();
+		p2.drawFighter();
+		
+		int i=0;
+		while(f1.getHealth()>0 && f2.getHealth()>0){
+			System.out.println("\nPlay cycle: "+i+"\n=============");	
+						
+			System.out.println(f1.getName());
+			f1.getBehavior().calculateActualMoves(f1.getLocationCondition()+" "+f1.getPowerCondition());
+			System.out.println("\n"+f2.getName());		
+			f2.getBehavior().calculateActualMoves(f2.getLocationCondition()+" "+f2.getPowerCondition());
+			f1.calculateCurrentAttackandMove();
+			f2.calculateCurrentAttackandMove();
+			
 			game.calculateHealth(f1, f2);
 			game.updatePowerCondition(f1, f2);
-			f1.getBehavior().calculateActualMoves(f1.getLocationCondition()+" "+f1.getPowerCondition());
-			f2.getBehavior().calculateActualMoves(f2.getLocationCondition()+" "+f2.getPowerCondition());
-			System.out.println("\nFighter 1=======\n");
-			f1.calculateCurrentAttackandMove();
-			System.out.println("\n\nFighter 2=======\n");
-			f2.calculateCurrentAttackandMove();
-			i++;
-		}
-	*/	
-		// System.out.println("Results:");
-		// System.out.println(fighters);
+			game.updateLocation(f1, f2);
 
+			System.out.println("\n"+f1.getName()+": Health="+f1.getHealth()+": Position="+f1.getLocation());
+			System.out.println("Condition: "+f1.getLocationCondition()+" "+f1.getPowerCondition());
+
+			System.out.println("\n"+f2.getName()+": Health="+f2.getHealth()+": Position="+f2.getLocation());
+			System.out.println("Condition: "+f2.getLocationCondition()+" "+f2.getPowerCondition());
+			
+			i++;
+			
+			/**Graphics**/
+			game.refreshScreen();
+			StdDraw.setPenColor(StdDraw.MAGENTA);
+			StdDraw.text(1, 9.0, f1.getName());
+			StdDraw.text(9.0, 9.0, f2.getName());
+			StdDraw.text(1.0, 9.5, "Health = "+f1.getHealth());
+			StdDraw.text(9.0, 9.5, "Health = "+f2.getHealth());
+			p1.updateFighterPosition(f1.getLocation());
+			p2.updateFighterPosition(f2.getLocation());
+			
+			game.chooseVisualization(p1,f1);
+			game.chooseVisualization(p2,f2);
+			
+			p1.drawFighter();
+			p2.drawFighter();			
+			/*****/
+			// display and pause for 20 ms
+			StdDraw.show(300);
+			p1.initializeBodyHandsLegs();
+			p2.initializeBodyHandsLegs();
+			game.refreshScreen();
+			StdDraw.setPenColor(StdDraw.MAGENTA);
+			StdDraw.text(1, 9.0, f1.getName());
+			StdDraw.text(9.0, 9.0, f2.getName());
+			StdDraw.text(1.0, 9.5, "Health = "+f1.getHealth());
+			StdDraw.text(9.0, 9.5, "Health = "+f2.getHealth());
+			p1.drawFighter();
+			p2.drawFighter();	
+			StdDraw.show(700);
+			
+			
+			if(f1.getHealth()<=0 && f2.getHealth()<=0){
+				System.out.println("\n=============\n=IT'S A TIE!=\n=============\n=============\n=GAME OVER ==\n=============\n");
+				break;//System.exit(1);				
+			}
+			if(f1.getHealth()<=0){
+				System.out.println("\n============\n"+f2.getName()+" WON\n============\n============\nGAME OVER ==\n============\n");
+				break;//System.exit(1);
+			}
+			if(f2.getHealth()<=0){
+				System.out.println("\n============\n"+f1.getName()+" WON\n============\n============\nGAME OVER ==\n============\n");
+				break;//System.exit(1);
+			}
+		}
+	}
+	
+	public void chooseVisualization(FighterVisualization f, Fighter fighter){
+		if(fighter.getCurrentMove().equals("crouch")){
+			f.moveUpDown(FighterVisualization.CROUCH);
+		}else if(fighter.getCurrentMove().equals("jump")){
+			f.moveUpDown(FighterVisualization.JUMP);
+		}
+		
+		if(fighter.getCurrentAttack().equals("punch_low")){
+			f.lowPunch();
+		}else if (fighter.getCurrentAttack().equals("punch_high")){
+			f.highPunch();
+		}else if (fighter.getCurrentAttack().equals("kick_low")){
+			f.lowKick();
+		}else if (fighter.getCurrentAttack().equals("kick_high")){
+			f.highKick();
+		}else if (fighter.getCurrentAttack().equals("block_high")){
+			f.block(FighterVisualization.HIGH);
+		}else if (fighter.getCurrentAttack().equals("block_low")){
+			f.block(FighterVisualization.LOW);
+		}
 	}
 
 }
