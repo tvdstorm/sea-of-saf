18ec02aa894736e25d0f7b19ecbb35ea567aef88
marin.vanbeek@gmail.com <marin.vanbeek@gmail.com@e25deb68-5119-325c-d63b-03bbf1c8524c>
2012-02-20 16:53:45 +0000

    Clean up.

--- trunk/marinvanbeek/src/saf/animation/FighterSprites.java
+++ trunk/marinvanbeek/src/saf/animation/FighterSprites.java
@@ -3,7 +3,6 @@ package saf.animation;
 import saf.data.*;
 
 import java.util.List;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Dictionary;
