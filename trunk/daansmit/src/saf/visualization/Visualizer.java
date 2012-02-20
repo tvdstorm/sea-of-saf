a607f666572cc0e4e344e2797c5d7bda48a5b17d
daansmit.uva@gmail.com <daansmit.uva@gmail.com@e25deb68-5119-325c-d63b-03bbf1c8524c>
2012-02-20 12:46:11 +0000

    text visualizer

---
+++ trunk/daansmit/src/saf/visualization/Visualizer.java
@@ -0,0 +1,24 @@
+package saf.visualization;
+
+import saf.simulation.event.*;
+
+public interface Visualizer
+{
+    public void visualize(Event event);
+    public void visualize(Block block);
+    public void visualize(BlockHigh blockHigh);
+    public void visualize(BlockLow blockLow);
+    public void visualize(ChangeHealth changeHealth);
+    public void visualize(Crouch crouch);
+    public void visualize(Hit hit);
+    public void visualize(Jump jump);
+    public void visualize(KickHigh kickHigh);
+    public void visualize(KickLow kickLow);
+    public void visualize(Miss miss);
+    public void visualize(MoveTo moveTo);
+    public void visualize(PunchHigh punchHigh);
+    public void visualize(PunchLow punchLow);
+    public void visualize(Stand stand);
+    public void visualize(StopAttack stopAttack);
+    public void visualize(Win win);
+}
