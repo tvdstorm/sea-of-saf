57d1f4dd1bbf6357d4a64e359f8acb2826599ed3
vlad.lep@gmail.com <vlad.lep@gmail.com@e25deb68-5119-325c-d63b-03bbf1c8524c>
2012-02-21 15:59:56 +0000

    modify in so the picture will not get out of the screen

--- vladlep/SuperAwesomeFighters/src/fighter/gui/ScenePanel.java
+++ vladlep/SuperAwesomeFighters/src/fighter/gui/ScenePanel.java
@@ -43,7 +43,7 @@ public class ScenePanel extends JPanel {
 
 		int positionX, positionY;
 		positionX = figterState.getPositionX();
-		positionX = mapCoordinateX(positionX);
+		positionX = mapCoordinateX(positionX,image.getWidth());
 		positionY = figterState.getPositionY();
 		positionY = mapCoordinateY(positionY);
 
@@ -51,9 +51,9 @@ public class ScenePanel extends JPanel {
 				positionY - image.getHeight() / 2, null);
 	}
 
-	private int mapCoordinateX(int battlePositionX) {
+	private int mapCoordinateX(int battlePositionX, int pictureWidht) {
 		int panelPosition;
-		panelPosition = (int) (battlePositionX * (double) (ArenaConstants.ARENA_WIDTH / BattleConstants.arenaLength));
+		panelPosition = (int) (battlePositionX * (double) ((ArenaConstants.ARENA_WIDTH - pictureWidht/2)/ BattleConstants.arenaLength));
 
 		return panelPosition;
 	}
