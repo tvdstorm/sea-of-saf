268089e6d24e5f4786d09cc5be0a92d08ffd7cce
job.jonkergouw@gmail.com <job.jonkergouw@gmail.com@e25deb68-5119-325c-d63b-03bbf1c8524c>
2012-03-06 11:10:03 +0000

    added new graphics

--- trunk/jobjonkergouw/src/saf/game/Game.java
+++ trunk/jobjonkergouw/src/saf/game/Game.java
@@ -1,69 +1,109 @@
 package saf.game;
 
+import saf.ErrorHandler;
+
+/**
+ * Let two saf bots fight with graphics or text.
+ * @author job
+ *
+ */
 public class Game {
 	private Bot bot1;
 	private Bot bot2;
 	
-	private final double GAME_WIDTH = 14.;
-	private final int SLEEPTIME = 500;	
+	private final double GAME_WIDTH = 15.;	// size of game area in game position units
+	private final int SLEEPTIME = 1000;		// determines real clock time per timestep
+	private final double dt	= 0.1;			// size of time increments
+	
+	private Window gameWindow;
+	boolean enableGUI;
+	boolean enableText;
 
+	/**
+	 * 
+	 * @param bot1	first bot
+	 * @param bot2  second bot
+	 */
 	public Game(Bot bot1, Bot bot2) {
 		this.bot1 = bot1;
 		this.bot2 = bot2;
 
-		bot1.setCurrentPosition(0.);
-		bot2.setCurrentPosition(GAME_WIDTH);
-	}
-
-	// run game with graphics
-	public void runGUI() {
-		Window w = new Window(GAME_WIDTH, bot1, bot2);
+		bot1.setGameWidth(GAME_WIDTH);
+		bot2.setGameWidth(GAME_WIDTH);
 		
-		do {
-			try {
-				Thread.sleep(SLEEPTIME);
-			} catch (java.lang.InterruptedException e) {
-				java.lang.System.exit(1);
-				e.printStackTrace();
-			}
-			
-			runTimeStep();
-			w.setBotStates(bot1, bot2);
-			w.updateScreen();			
-		} while (!bot1.isDead() && !bot2.isDead() );
+		bot1.setCurrentPosition(5.);
+		bot2.setCurrentPosition(15);
+		
+		enableGUI = false;
+		enableText = false;
 	}
 
-	// run a text based version
+	/**
+	 * Run the game with graphics only
+	 */
+	public void runGUI() {
+		enableGUI = true;
+		enableText = false;		
+		runGame();
+	}
+	
+	/**
+	 * Run the game with text only
+	 */
 	public void runText() {
-		do {
+		enableGUI = false;
+		enableText = true;		
+		runGame();		
+	}
+	
+	/**
+	 * Run the game by iterating over time
+	 */
+	private void runGame() {
+		if (enableGUI) {
+			gameWindow = new Window(GAME_WIDTH, bot1, bot2);
+		}
+		if (enableText) {
+			System.out.println( bot1.botSummaryAsString() );
+			System.out.println( bot2.botSummaryAsString() );			
+		}
+
+		for (double t=0; !bot1.isDead() && !bot2.isDead(); t += dt ) {
 			try {
 				Thread.sleep(SLEEPTIME);
 			} catch (java.lang.InterruptedException e) {
-				java.lang.System.exit(1);
-				e.printStackTrace();
+				ErrorHandler.exitWithException(e);
 			}
-			runTimeStep();
-			System.out.println(bot1.botStateAsString());
-			System.out.println(bot2.botStateAsString());
-		} while (!bot1.isDead() && !bot2.isDead() );
-	}
-
-	private void runTimeStep() {
-			bot1.compareStrengthConditions(bot2);
-			bot2.compareStrengthConditions(bot1);
-			bot1.compareRangeConditions(bot2);
-			bot2.compareRangeConditions(bot1);
-
-			bot1.determineMoveAndAttackByRules();
-			bot2.determineMoveAndAttackByRules();
-
-			bot1.moveOtherAccordingToState(bot2);
-			bot2.moveOtherAccordingToState(bot1);
-
-			bot1.attackOtherAccordingToState(bot2);
-			bot2.attackOtherAccordingToState(bot1);
 			
-			bot2.checkIfDead();
-			bot2.checkIfDead();
+			checkAndUpdateBot(t, bot1, bot2);
+			checkAndUpdateBot(t, bot2, bot1);			
+		}
+		
+
 	}
+	
+	/**
+	 * Check if its time for botA to perform actions
+	 * @param t 	= the time
+	 * @param botA	= the bot
+	 * @param botB  = his opponent
+	 */
+	private void checkAndUpdateBot(double t, Bot botA, Bot botB) {
+		if ( botA.isTimeForNextAction(t) ) {
+			botA.updateBotActions(botB);
+			botA.updateTimeForNextAction();
+			
+			if (enableGUI) {
+				gameWindow.setBotStates(botA, botB);
+				gameWindow.updateScreen();
+			}
+			if (enableText) {
+				System.out.println("Bot states at t=" + t);
+				System.out.print(bot1.botStateAsString());
+				System.out.println(bot2.botStateAsString());					
+			}
+		}		
+	}
+
+
 }
