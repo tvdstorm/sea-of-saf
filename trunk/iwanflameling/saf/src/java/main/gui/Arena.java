3ed129d6a422914c354bdd21a578defe23a641b7
iwanovich@gmail.com <iwanovich@gmail.com@e25deb68-5119-325c-d63b-03bbf1c8524c>
2013-05-25 15:21:45 +0000

    Renamed Battlefield to Arena, is better suited, I guess

--- trunk/iwanflameling/saf/src/java/main/gui/Arena.java
+++ trunk/iwanflameling/saf/src/java/main/gui/Arena.java
@@ -2,7 +2,7 @@ package gui;
 
 import javax.swing.JFrame;
 
-public class Battlefield extends JFrame implements UpdatableGui {
+public class Arena extends JFrame implements UpdatableGui {
 
 	public void init(){
 		
