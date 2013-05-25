7386a8d33327a7a279671552d6fa9803337c57dd
iwanovich@gmail.com <iwanovich@gmail.com@e25deb68-5119-325c-d63b-03bbf1c8524c>
2013-05-25 15:20:11 +0000

    Renamed Arena to Jury, is better suited, I guess

---
+++ iwanflameling/saf/src/java/main/gui/Battlefield.java
@@ -0,0 +1,16 @@
+package gui;
+
+import javax.swing.JFrame;
+
+public class Battlefield extends JFrame implements UpdatableGui {
+
+	public void init(){
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+}
